66faa7f1fc18e8a7eb7abf024fbfb1b50676be65
grenaad <dan4rie@gmail.com>
2023-08-31 11:23:57 +0200

    added auth

--- src/API/Program.fs
+++ src/API/Program.fs
@@ -12,34 +12,52 @@ open Microsoft.AspNetCore.Authentication.Cookies
 open Microsoft.AspNetCore.Hosting
 open Microsoft.Extensions.Logging
 open Microsoft.Extensions.Hosting
-let configureServices (services: IServiceCollection) =
+open System.Security.Claims
+open Microsoft.AspNetCore.Authorization
 
+[<Literal>] 
+let accessDeniedPath = "/forbidden"
+
+let configureServices (services: IServiceCollection) =
   services
-    .AddAuthentication(fun opt -> opt.DefaultAuthenticateScheme <- CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddAuthentication(fun opt -> 
+    opt.DefaultAuthenticateScheme <- CookieAuthenticationDefaults.AuthenticationScheme)
     .AddCookie(fun options ->
       options.ExpireTimeSpan <- TimeSpan.FromMinutes 20
       options.SlidingExpiration <- true
-      options.AccessDeniedPath <- "/forbidden/")
+      options.AccessDeniedPath <- accessDeniedPath)
   |> ignore
+  services.AddAuthorization(fun config ->
+            let adminPolicy = (new AuthorizationPolicyBuilder()).RequireClaim(ClaimTypes.Role,["Admin"]).Build()
+            let userPolicy = (new AuthorizationPolicyBuilder()).RequireClaim(ClaimTypes.Role,["User"]).Build()
+            config.AddPolicy("Admin",adminPolicy)
+            config.AddPolicy("User",userPolicy)
+            ) |> ignore
+
+  services.AddAuthorization(fun (options ) ->
+        options.AddPolicy( "AdminPolicy", fun policy -> 
+            policy.RequireClaim(ClaimTypes.Role, "Admin") |> ignore 
+            // you can require other claims as well here
+        ) |> ignore
+    ) |> ignore
 
 let configureApp (app: IApplicationBuilder) =
-  if not (app.ApplicationServices.GetService<IWebHostEnvironment>().IsDevelopment()) then
-      printfn "Development"
-    //   app.UseExceptionHandler("/Error")
-    //   app.UseHsts()
+  let isDev = app.ApplicationServices.GetService<IWebHostEnvironment>().IsDevelopment()
+  if not (isDev) then
+      printfn "Production"
+      app.UseExceptionHandler("/Error") |> ignore
+      app.UseHsts() |> ignore
   // app.UseHttpsRedirection() |> ignore
   // app.UseStaticFiles() |> ignore
 
   app.UseAuthentication() |> ignore
-// app.UseAuthorization() |> ignore
+  app.UseAuthorization() |> ignore
 
 let configureLogging (builder: ILoggingBuilder) =
   let filter (l: LogLevel) = l.Equals LogLevel.Error
   builder.AddFilter(filter).AddConsole().AddDebug() |> ignore
 
 let builder = WebApplication.CreateBuilder()
-
-// builder.Services.AddSingleton<ILoggerFactory>(LoggerFactory.Create(builder.Configuration.GetSection "Logging")) |> ignore
 
 configureServices builder.Services
 
@@ -54,19 +72,37 @@ let strToHtml s =
 
 let reactElementToHtml s = s |> reactElementTostr |> strToHtml
 
+let get (path: string) (response: Func<_>) (autPolicyName: string option ) = 
+    match autPolicyName with
+    | Some name -> app.MapGet(path, response).RequireAuthorization(name) |> ignore
+    | None -> app.MapGet(path, response) |> ignore
 
-let get (path: string) (response: Func<_>) = app.MapGet(path, response) |> ignore
+let post (path: string) (response: Func<_>) (autPolicyName: string option )= 
+    match autPolicyName with
+    | Some name -> app.MapPost(path, response).RequireAuthorization(name) |> ignore
+    | None -> app.MapGet(path, response) |> ignore
 
-let post (path: string) (response: Func<_>) = app.MapPost(path, response) |> ignore
+(*
+let authorizeByPolicyName (app: IApplicationBuilder) (policyName : string) (authFailedHandler : HttpHandler) : HttpHandler =
+    fun (next : HttpFunc) (ctx : HttpContext) ->
+        task {
+            let authService =  app.ApplicationServices.GetService<IAuthorizationService>()
+            let! result = authService.AuthorizeAsync (ctx.User, policyName)
+            return! (if result.Succeeded then next else authFailedHandler earlyReturn) ctx
+        }
 
-get "/index.html" (strToHtml indexPage)
+let requireAdminPolicy : HttpHandler = 
+    authorizeByPolicyName "AdminPolicy" (RequestErrors.FORBIDDEN  "Permission denied. Failed Admin Policy")
+*)
 
-get "/login" (reactElementToHtml loginComponent)
+get "/index.html" (strToHtml indexPage) None
 
-get "/main" (reactElementToHtml (mainLayout loginComponent))
+get "/login" (reactElementToHtml loginComponent) None
 
-// TODO: need to component
-get "/forbidden" (reactElementToHtml (loginComponent))
+get "/main" (reactElementToHtml (mainLayout loginComponent)) (Some "Admin")
+
+// TODO: need component
+get accessDeniedPath (reactElementToHtml (loginComponent)) None
 
 // post "/post" (strToHtml indexPage)
 
--- src/Views/Components/LoginComponent.fs
+++ src/Views/Components/LoginComponent.fs
@@ -5,56 +5,39 @@ open Feliz.ViewEngine
 // open type prop
 
 let loginComponent =
-  Html.div [
-      prop.classes [ "flex"; "h-screen"; "justify-center"; "items-center" ]
-      prop.children [
-          Html.div [
-              prop.classes [ "bg-white"; "rounded-lg"; "p-8"; "shadow-md"; "w-96" ]
-              prop.children [
-                  Html.h2 [
-                      prop.classes [ "text-2xl"; "font-semibold"; "mb-4" ]
-                      prop.text "Login"
-                  ]
-                  Html.form [
-                      Html.div [
-                          prop.className "mb-4"
-                          prop.children [
-                              Html.label [
-                                  prop.classes [ "block"; "font-medium"; "mb-1"; "text-black" ]
+  Html.div
+    [ prop.classes [ "flex"; "h-screen"; "justify-center"; "items-center" ]
+      prop.children
+        [ Html.div
+            [ prop.classes [ "bg-white"; "rounded-lg"; "p-8"; "shadow-md"; "w-96" ]
+              prop.children
+                [ Html.h2 [ prop.classes [ "text-2xl"; "font-semibold"; "mb-4" ]; prop.text "Login" ]
+                  Html.form
+                    [ Html.div
+                        [ prop.className "mb-4"
+                          prop.children
+                            [ Html.label
+                                [ prop.classes [ "block"; "font-medium"; "mb-1"; "text-black" ]
                                   prop.for' "username"
-                                  prop.text "Username"
-                              ]
-                              Html.input [
-                                  prop.type' "text"
+                                  prop.text "Username" ]
+                              Html.input
+                                [ prop.type' "text"
                                   prop.id "username"
-                                  prop.classes [ "w-full"; "p-2"; "rounded"; "border"; "bg-gray-100" ; "text-black"  ]
-                                  prop.required true
-                              ]
-                          ]
-                      ]
-                      Html.div [
-                          prop.className "mb-4"
-                          prop.children [
-                              Html.label [
-                                  prop.classes [ "block"; "font-medium"; "mb-1" ; "text-black"]
+                                  prop.classes [ "w-full"; "p-2"; "rounded"; "border"; "bg-gray-100"; "text-black" ]
+                                  prop.required true ] ] ]
+                      Html.div
+                        [ prop.className "mb-4"
+                          prop.children
+                            [ Html.label
+                                [ prop.classes [ "block"; "font-medium"; "mb-1"; "text-black" ]
                                   prop.for' "password"
-                                  prop.text "Password"
-                              ]
-                              Html.input [
-                                  prop.type' "password"
+                                  prop.text "Password" ]
+                              Html.input
+                                [ prop.type' "password"
                                   prop.id "password"
-                                  prop.classes [ "w-full"; "p-2"; "rounded"; "border"; "bg-gray-100" ; "text-black"]
-                                  prop.required true
-                              ]
-                          ]
-                      ]
-                      Html.button [
-                          prop.classes [ "w-full"; "btn"; "btn-primary"; "text-black" ]
+                                  prop.classes [ "w-full"; "p-2"; "rounded"; "border"; "bg-gray-100"; "text-black" ]
+                                  prop.required true ] ] ]
+                      Html.button
+                        [ prop.classes [ "w-full"; "btn"; "btn-primary"; "text-black" ]
                           prop.type' "submit"
-                          prop.text "Login"
-                      ]
-                  ]
-              ]
-          ]
-      ]
-  ]
+                          prop.text "Login" ] ] ] ] ] ]
